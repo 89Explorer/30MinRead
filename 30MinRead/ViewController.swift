ca08ee9a9c4f8d445b23186a052dcb3ae72cfc82
89Explorer <leral3@naver.com>
2025-03-13 11:05:47 +0900

    Initial Commit

---
+++ 30MinRead/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  30MinRead
+//
+//  Created by 권정근 on 3/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 30MinReadTests/_0MinReadTests.swift
@@ -0,0 +1,17 @@
+//
+//  _0MinReadTests.swift
+//  30MinReadTests
+//
+//  Created by 권정근 on 3/13/25.
+//
+
+import Testing
+@testable import _0MinRead
+
+struct _0MinReadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
